f60963e7c191b5425188221019a2b0ea412fbcac
edwin1004 <80591163+edwin1004@users.noreply.github.com>
2022-08-11 14:30:23 +0800

    Update Assignment1_ChanChunHeiEdwin.py

--- Assignment1_ChanChunHeiEdwin.py
+++ Assignment1_ChanChunHeiEdwin.py
@@ -2,7 +2,7 @@ Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53)
 [Clang 6.0 (clang-600.0.57)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> Amount = int(input("Enter the amount:"))
-Enter the amount: 93
+Enter the amount: 100
 >>> print("Quarters =", Amount // 25)
 Quarters = 3
 >>> Amount = Amount % 25
